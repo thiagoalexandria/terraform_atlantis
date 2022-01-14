2a7df1c14cba329260fbbaf1bd06b44cd8ae5dc0
Thiago Oliveira de Alexandria <CIANDT\thiagooa@lnb029322bhz.ciandt.global>
2022-01-14 09:17:05 -0300

    teste

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-tfstate-atlantis-demo"
+    key            = "atlantis/demo.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
+resource "aws_s3_bucket" "atlantis_demo" {
+    bucket = "thiago-atlantis-demo-site"
+    acl    = "private"
+
+  tags = {
+    Name        = "Atlantis demo"
+    Environment = "Dev"
+  }
+}
